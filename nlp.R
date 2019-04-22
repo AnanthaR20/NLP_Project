36b2abbacff79f04aa99f35a5a4cf31ab97d242f
Anantha Rao <anrao3@ad.sohe.wisc.edu>
2019-04-22 16:39:57 -0500

    Added matrix of distributions over bad, medium and good ratings, per city

--- nlp.R
+++ nlp.R
@@ -1,19 +1,26 @@
 # NLP Project #
 rm(list=ls())
-data.test <- read.csv("class_cleaned_data/test_Madison.csv")
 data.train <- read.csv("class_cleaned_data/train_Madison.csv")
-attach(data.train)
 d <- data.train
+attach(d)
+
 
 model <- lm(star ~ postal_code + incredible + awful + amazing + poor + waited + disappointed + city)
 
 #Search
 cities <- factor(d$city)
+region <- table(cities, star)
+city.rating <- matrix(nrow = nrow(region), ncol = 3)
 
-region.dist <- table(cities, star)
-
-
-
-
+for(town in 1:nrow(region)){
+  no.good <- region[town,4]+region[town,5]
+  no.med <- region[town,3]
+  no.bad <- region[town,1]+region[town,2]
+  no.total <- sum(region[town,])
+  
+  city.rating[town,1] <- no.bad/no.total
+  city.rating[town,2] <- no.med/no.total
+  city.rating[town,3] <- no.good/no.total
+}
 
 
